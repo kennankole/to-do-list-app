f2bb1e036715b368afa9e86e0a2fd369fc072cb8
kennankole <kennankole@gmail.com>
2022-10-06 15:18:37 +0300

    Implemented to do list CRD -Create -Read -Delete Styled the to do list

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import _, { times } from 'lodash';
+import _, { add, times } from 'lodash';
 import './style.css';
+
 
 const tasks = [];
 const elemList = [];
-
 const formContainer = document.getElementById('form')
 const inputElement = document.getElementById('input-element');
 const myContainer = document.getElementById('items-container');
-const footerContainer = document.getElementById('to-do-container')
 const clearAll = document.getElementById('footer-a')
+const data = JSON.parse(localStorage.getItem('listItems'));
 
-formContainer.addEventListener('submit', (e) => {
-	e.preventDefault();
+
+// Add items
+export const toDoList = () => {
 	elemList.push(inputElement.value);
 	const listObj = {
 		description: inputElement.value,
 		completed: false,
 		index: elemList.length,
 	};
-	
+
 	tasks.push(listObj);
 	localStorage.setItem('listItems', JSON.stringify(tasks));
 	myContainer.innerHTML += `
@@ -31,13 +32,13 @@ formContainer.addEventListener('submit', (e) => {
 	`;
 	inputElement.value = '';
 	window.location.reload;
-});
+}
 
 
-const data = JSON.parse(localStorage.getItem('listItems'));
-function showData(){
+// Display data
+function showData() {
 	data.forEach((item, index) => {
-	myContainer.innerHTML += `
+		myContainer.innerHTML += `
 	<li class='list-container'>
 		<div class='list-text'>
 			<input type="checkbox" class="check">
@@ -45,49 +46,55 @@ function showData(){
 		</div>
 	</li>
 	`;
-	clearAll.addEventListener('click', () => {
-		deleteItems(index);
-	});
-
 	});
 }
 
-
-
-if (localStorage.getItem('listItems') !== null){
+if (localStorage.getItem('listItems') !== null) {
 	showData()
-}else {
-	localStorage.setItem('listItems', JSON.stringify(tasks));
+} else {
+	localStorage.setItem('listItems', JSON.stringify(data));
 }
 
 // Check and uncheck items.
 const listItem = document.getElementsByClassName('list-text');
-
 const checked = document.getElementsByClassName('check');
 const elem = Array.from(checked);
-// elem.forEach((item, index) => {
-// 	item.addEventListener('change', (e) => {
-// 		console.log(e.currentTarget.checked)
-// 		if(e.currentTarget.checked){
-// 			listItem[index].style.textDecoration = 'line-through';
-// 			console.log(index)
-// 		}else{
-// 			listItem[index].style.textDecoration = 'none';
-// 		}
-// 	})
-// })
+elem.forEach((item, index) => {
+	item.addEventListener('change', (e) => {
+		console.log(e.currentTarget.checked)
+		if (e.currentTarget.checked) {
+			listItem[index].style.textDecoration = 'line-through';
+		} else {
+			listItem[index].style.textDecoration = 'none';
+		}
+	})
+})
 
-
-
-
+//Delete  items
 const items = Array.from(listItem);
-
-function deleteItems(index) {
-	data.splice(index, 1)
-	localStorage.setItem('listItems', JSON.stringify(tasks));
-	window.location.reload;
+function deleteItems() {
+	const listElem = document.querySelectorAll('input:checked');
+	if (listElem.length > 0) {
+		items.forEach((item, index) => {
+			if (item.style.textDecoration === 'line-through') {
+				item.remove()
+				document.location.reload(true);
+				data.splice(index, listElem.length)
+				localStorage.setItem('listItems', JSON.stringify(data));
+			} else {
+				return;
+			}
+		})
+	} else {
+		return;
+	}
 }
 
+formContainer.addEventListener('submit', (e) => {
+	e.preventDefault();
+	toDoList();
+});
 
-// clearAll.addEventListener('click', deleteItems)
+clearAll.addEventListener('click', deleteItems)
+
 
